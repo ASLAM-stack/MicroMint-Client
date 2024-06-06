fb9c2b335f7c63b64d0de8ef879ab10adb3afb38
ASLAM-stack <arifhossainaslam6@gmail.com>
2024-06-06 13:12:43 +0600

    add Navbar

--- src/Pages/Share/Navber.jsx
+++ src/Pages/Share/Navber.jsx
@@ -1,8 +1,9 @@
 import { NavLink } from "react-router-dom";
 
 const Navber = () => {
+  const user = false
   return (
-    <div>
+    <div className="shadow">
       <div className="navbar bg-base-100">
         <div className="navbar-start">
           <div className="dropdown">
@@ -26,7 +27,27 @@ const Navber = () => {
               tabIndex={0}
               className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52"
             >
-                
+               <li>
+                    <NavLink to='/'>Home</NavLink>
+                </li>
+                <li>
+                    <a href='#about'>About</a>
+                </li>
+                <li>
+                    <a href='#about'>Features</a>
+                </li>
+                <li>
+                    <a href='#about'>How it&apos;s work</a>
+                </li>
+                <li>
+                    <a href='#about'>Top Worker</a>
+                </li>
+                <li>
+                    <a href='#about'>Review</a>
+                </li>
+                <li>
+                <a className="btn mt-1  btn-outline btn-secondary btn-sm">Watch Demo</a>
+                </li>
            
             </ul>
           </div>
@@ -34,30 +55,57 @@ const Navber = () => {
         </div>
         <div className="navbar-center hidden lg:flex">
           <ul className="menu menu-horizontal px-1">
-                 
+                <li>
+                    <NavLink to='/'>Home</NavLink>
+                </li>
+                <li>
+                    <a href='#about'>About</a>
+                </li>
+                <li>
+                    <a href='#about'>Features</a>
+                </li>
+                <li>
+                    <a href='#about'>How it&apos;s work</a>
+                </li>
+                <li>
+                    <a href='#about'>Top Worker</a>
+                </li>
+                <li>
+                    <a href='#about'>Review</a>
+                </li>
+                <li>
+                <a className="btn mt-1  btn-outline btn-secondary btn-sm">Watch Demo</a>
+                </li>
        
           </ul>
         </div>
         <div className="navbar-end">
-        <div className="dropdown dropdown-end">
-      <div tabIndex={0} role="button" className="btn btn-ghost btn-circle avatar">
-        <div className="w-10 rounded-full">
-          <img alt="Tailwind CSS Navbar component" src="https://img.daisyui.com/images/stock/photo-1534528741775-53994a69daeb.jpg" />
+        {
+          user ? <div className="dropdown dropdown-end">
+          <div tabIndex={0} role="button" className="btn btn-ghost btn-circle avatar">
+            <div className="w-10 rounded-full">
+              <img alt="Tailwind CSS Navbar component" src="https://img.daisyui.com/images/stock/photo-1534528741775-53994a69daeb.jpg" />
+            </div>
+          </div>
+          <ul tabIndex={0} className="menu menu-sm dropdown-content mt-3 z-50 p-2 shadow bg-base-100 rounded-box w-52">
+            <li>
+              <a className="justify-between">
+                Profile
+                <span className="badge">New</span>
+              </a>
+            </li>
+            <li><a>DashBoard</a></li>
+            <li><a>Available Coins
+            <span className="badge">0</span>
+              </a></li>
+            <li><a>Logout</a></li>
+          </ul>
         </div>
-      </div>
-      <ul tabIndex={0} className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52">
-        <li>
-          <a className="justify-between">
-            Profile
-            <span className="badge">New</span>
-          </a>
-        </li>
-        <li><a>DashBoard</a></li>
-        <li><a>Logout</a></li>
-      </ul>
-    </div>
+        :
+        <a className="btn ml-5 btn-outline btn-secondary">Log in</a>
+        }
            {/* log in */}
-          <a className="btn ml-5 btn-outline btn-secondary">Log in</a>
+          
         </div>
       </div>
     </div>
